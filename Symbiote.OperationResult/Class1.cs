58d500978c6ffccaec722f481b22d40f3b3d1686
JP Dillingham <jpdillingham@gmail.com>
2016-06-25 18:19:32 -0500

    Add project files.

---
+++ Symbiote.OperationResult/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symbiote.OperationResult
+{
+    public class Class1
+    {
+    }
+}
